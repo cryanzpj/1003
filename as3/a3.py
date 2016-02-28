6ebaf2f46b0a9da94c4052f66749114743362d26
cryanzpj <cryanzpj@gmail.com>
2016-02-28 15:53:24 -0500

    updated 2/26

--- as3/a3.py
+++ as3/a3.py
@@ -1,4 +1,4 @@
-import util
+from util import *
 from joblib import Parallel, delayed
 import matplotlib.pyplot as plt
 import multiprocessing
@@ -176,7 +176,7 @@ def list_feature(X,w,n):
         temp[i] = abs(j*X[i])
     res = np.zeros((4,n),dtype=object)
     for k,i in enumerate(temp.most_common(n)):
-        res[:,k] = i[0],X[i[0]],w[i[0]],X[i[0]] *w[i[0]]
+        res[:,k] = i[0],X[i[0]],w[i[0]],abs(X[i[0]] *w[i[0]])
     return res
 
 if __name__ == '__main__':
@@ -187,7 +187,7 @@ if __name__ == '__main__':
     y_test  = np.load("Y_test.npy")
 
 
-    w1 = pegasos_svm_sgd(X_train,y_train,1,1)  # 35s
+    w1 = pegasos_svm_sgd(X_train,y_train,1,1,print_time=True)  # 35s
     l1 = loss_0_1(X_train,y_train,w1)
 
     w2 = pegasos_svm_sgd_2(X_train,y_train,1,1) # 0.38 s
@@ -201,21 +201,44 @@ if __name__ == '__main__':
         loss_list[i] = loss_0_1(X_test,y_test,w)
 
 
-
+    ###6.7
     try_list_2 = np.power(10.0,np.linspace(-5,-4,20))
     loss_list_2 = np.zeros(20)
     for i,j in enumerate(try_list_2):
-        w = pegasos_svm_sgd_2(X_train,y_train,lambda_ = j,n_ite = 20)
+        w = pegasos_svm_sgd_2(X_train,y_train,lambda_ = j,n_ite = 40)
         loss_list_2[i] = loss_0_1(X_test,y_test,w)
 
     lambda_opt = try_list_2[np.where(loss_list_2 == min(loss_list_2))[0][0]]
-    w_opt = pegasos_svm_sgd_2(X_train,y_train,lambda_ = lambda_opt,n_ite = 20)
+    w_opt = pegasos_svm_sgd_2(X_train,y_train,lambda_ = lambda_opt,n_ite = 40,print_time= True)
 
     X = np.array(map(lambda a: tokenlizer(a) , X_test),dtype = object)
     y = y_test
-    score = np.array(map(lambda t: dotProduct(w_opt,t),X))
-    prediction = map(lambda t: 1 if t>0 else 0, 1- np.array(map(lambda t: dotProduct(w,t),X))*y)
-    score_list = np.vstack((np.array(score),np.array(prediction))).T
+    score = np.abs(np.array(map(lambda t: dotProduct(w_opt,t),X)))
+    error =np.array( map(lambda t: 0 if t>0 else 1, np.array(map(lambda t: dotProduct(w,t),X))*y))
+    score_list = np.array([score,error]).T
+    score_list_sorted = score_list[score_list[:,0].argsort()]
+    temp =np.linspace(np.min(score),np.max(score),7)
+    index = map(lambda i:np.where(score_list_sorted[:,0] <= i )[0][-1] ,temp)
+    range_by_index = score_list_sorted[index][:,0]
+    error_by_index = map(lambda i:np.mean(score_list_sorted[:,1][index[i]:index[i+1]]),[0,1,2,3,4,5])
+    new_xstick = map(lambda i: str(range_by_index[i])[0:5]+ ' to ' +  str(range_by_index[i+1])[0:5] ,[0,1,2,3,4,5])
+    plt.close('all')
+    fig =plt.figure()
+    ax1 = fig.add_subplot(111)
+    ax1.bar([3,6,9,12,15,18],error_by_index)
+    sti_loc = np.array([3,6,9,12,15,18])+0.5
+    ax1.set_xticks(sti_loc)
+    ax1.set_xticklabels(new_xstick,size = 'x-small')
+    ax1.set_xlim([1,19])
+    ax1.set_xlabel("magnitude of the absolute score")
+    ax1.set_ylabel('percentage error')
+    plt.show()
+    plt.savefig('6_7.png')
+
+    ###6.8
+    pegasos_svm_sgd_2(X_train,y_train,lambda_ = lambda_opt,n_ite = 40,counter = True)
+
+
 
     #7
     error_list = np.where(np.array(prediction) ==1)[0][:4]
@@ -223,6 +246,6 @@ if __name__ == '__main__':
 
     res = []
     for i in txt_error:
-        res.extend(list_feature(i,w_opt,5))
+        res.append(list_feature(i,w_opt,5))
     res = np.array(res)
     #8
